56cc7187f298778014dfbba72a4d9106e954b8fa
yoksel <yokselzok@gmail.com>
2020-01-09 17:24:08 +0300

    Add a little more tests

--- components/rangePicker/test.js
+++ components/rangePicker/test.js
@@ -35,10 +35,50 @@ describe("RangePicker", () => {
 
   it("Shows selected dates from-to in input", () => {
     document.querySelector('.rangepicker__input').dispatchEvent(new MouseEvent("click"));
-    expect(document.querySelector('.rangepicker__input').textContent).toMatch(/02.10.2019/);
-    expect(document.querySelector('.rangepicker__input').textContent).toMatch(/05.11.2019/);
+    expect(document.querySelector('.rangepicker__input span[data-elem="from"]').textContent).toMatch(/02.10.2019/);
+    expect(document.querySelector('.rangepicker__input span[data-elem="to"]').textContent).toMatch(/05.11.2019/);
   });
 
+  it("Select start date", () => {
+    document.querySelector('.rangepicker__input').dispatchEvent(new MouseEvent("click"));
+    document.querySelector('.rangepicker__cell[data-value*="2019-11-27"]').dispatchEvent(new MouseEvent("click", {'bubbles': true}));
+    expect(document.querySelector('.rangepicker__cell.rangepicker__selected-from').textContent).toMatch(/28/);
+  });
 
-  // ...
+  it("Select finish date", () => {
+    document.querySelector('.rangepicker__input').dispatchEvent(new MouseEvent("click"));
+    document.querySelector('.rangepicker__cell[data-value*="2019-11-27"]').dispatchEvent(new MouseEvent("click", {'bubbles': true}));
+    document.querySelector('.rangepicker__cell[data-value*="2019-10-09"]').dispatchEvent(new MouseEvent("click", {'bubbles': true}));
+    expect(document.querySelector('.rangepicker__cell.rangepicker__selected-from').textContent).toMatch(/10/);
+  });
+
+  it("Shows selected dates from-to in input after choice", () => {
+    document.querySelector('.rangepicker__input').dispatchEvent(new MouseEvent("click"));
+    document.querySelector('.rangepicker__cell[data-value*="2019-11-27"]').dispatchEvent(new MouseEvent("click", {'bubbles': true}));
+    document.querySelector('.rangepicker__cell[data-value*="2019-10-09"]').dispatchEvent(new MouseEvent("click", {'bubbles': true}));
+    expect(document.querySelector('.rangepicker__input span[data-elem="from"]').textContent).toMatch(/10.10.2019/);
+    expect(document.querySelector('.rangepicker__input span[data-elem="to"]').textContent).toMatch(/28.11.2019/);
+  });
+
+  it("Select prev month", () => {
+    document.querySelector('.rangepicker__input').dispatchEvent(new MouseEvent("click"));
+    document.querySelector('.rangepicker__selector-control-left').dispatchEvent(new MouseEvent("click"));
+    expect(document.querySelector('.rangepicker__calendar .rangepicker__month-indicator').textContent).toMatch(/сентябрь/);
+  });
+
+  it("Select next month", () => {
+    document.querySelector('.rangepicker__input').dispatchEvent(new MouseEvent("click"));
+    document.querySelector('.rangepicker__selector-control-right').dispatchEvent(new MouseEvent("click"));
+    expect(document.querySelector('.rangepicker__calendar .rangepicker__month-indicator').textContent).toMatch(/ноябрь/);
+  });
+
+  it("Select date in prev month and check dates in from-to", () => {
+    document.querySelector('.rangepicker__input').dispatchEvent(new MouseEvent("click"));
+    document.querySelector('.rangepicker__selector-control-left').dispatchEvent(new MouseEvent("click"));
+    expect(document.querySelector('.rangepicker__calendar .rangepicker__month-indicator').textContent).toMatch(/сентябрь/);
+    document.querySelector('.rangepicker__cell[data-value*="2019-09-01"]').dispatchEvent(new MouseEvent("click", {'bubbles': true}));
+    document.querySelector('.rangepicker__cell[data-value*="2019-10-29"]').dispatchEvent(new MouseEvent("click", {'bubbles': true}));
+    expect(document.querySelector('.rangepicker__input span[data-elem="from"]').textContent).toMatch(/02.09.2019/);
+    expect(document.querySelector('.rangepicker__input span[data-elem="to"]').textContent).toMatch(/30.10.2019/);
+  });
 });
